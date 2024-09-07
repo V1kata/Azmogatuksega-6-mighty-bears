13383f6a957605fad767f642e30c91e7e11542b7
V1kata <viktor.burboran@gmail.com>
2024-09-07 18:58:11 +0300

    bug fix

--- Furniro-react/vite.config.js
+++ Furniro-react/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Azmogatuksega-6-mighty-bears/',
+  base: '/Azmogatuksega-6-mighty-bears/Furniro-react/',
   plugins: [react()],
 })
